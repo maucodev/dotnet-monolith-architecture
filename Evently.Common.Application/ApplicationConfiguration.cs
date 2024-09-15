2fc0c1ff0e85b60f1acdcc8eae0642e664789cfa
Mauricio Estrada <hola@maucodev.com>
2024-09-15 11:53:00 -0600

    Implement Validation With FluentValidation

--- Evently.Common.Application/ApplicationConfiguration.cs
+++ Evently.Common.Application/ApplicationConfiguration.cs
@@ -16,6 +16,8 @@ public static class ApplicationConfiguration
             config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
 
             config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+
+            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssemblies(moduleAssemblies, includeInternalTypes: true);
