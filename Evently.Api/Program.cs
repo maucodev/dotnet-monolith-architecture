4ea8f2d9c271943a7c83018e5063cf726dab74d9
Mauricio Estrada <hola@maucodev.com>
2024-09-16 13:32:11 -0600

    Scaffolding the Ticketing Module

--- Evently.Api/Program.cs
+++ Evently.Api/Program.cs
@@ -4,6 +4,7 @@ using Evently.Common.Application;
 using Evently.Common.Infrastructure;
 using Evently.Common.Presentation.Endpoints;
 using Evently.Modules.Events.Infrastructure;
+using Evently.Modules.Ticketing.Infrastructure;
 using Evently.Modules.Users.Infrastructure;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -31,7 +32,8 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddApplication([
     Evently.Modules.Events.Application.AssemblyReference.Assembly,
-    Evently.Modules.Users.Application.AssemblyReference.Assembly
+    Evently.Modules.Users.Application.AssemblyReference.Assembly,
+    Evently.Modules.Ticketing.Application.AssemblyReference.Assembly
 ]);
 
 builder.Services.AddInfrastructure(
@@ -45,11 +47,13 @@ builder.Services
 
 builder.Configuration.AddModuleConfiguration([
     "events", 
-    "users"
+    "users",
+    "ticketing"
 ]);
 
 builder.Services.AddEventsModule(builder.Configuration);
 builder.Services.AddUsersModule(builder.Configuration);
+builder.Services.AddTicketingModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
