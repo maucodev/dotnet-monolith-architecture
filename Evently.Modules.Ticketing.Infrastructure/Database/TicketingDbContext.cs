a4ce01d104cd1cae1474cda5c97456e14a994dec
Mauricio Estrada <hola@maucodev.com>
2024-09-16 18:10:20 -0600

    Add ticketing migration

---
+++ Evently.Modules.Ticketing.Infrastructure/Database/TicketingDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Evently.Modules.Ticketing.Domain.Customers;
+using Evently.Modules.Ticketing.Infrastructure.Customers;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Threading.Tasks;
+using System.Threading;
+using Evently.Modules.Ticketing.Application.Abstractions.Data;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Evently.Modules.Ticketing.Infrastructure.Database;
+
+public sealed class TicketingDbContext(
+    DbContextOptions<TicketingDbContext> options)
+    : DbContext(options), IUnitOfWork
+{
+    internal DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Ticketing);
+
+        modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+    }
+
+    public async Task<DbTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (Database.CurrentTransaction is not null)
+        {
+            await Database.CurrentTransaction.DisposeAsync();
+        }
+
+        return (await Database.BeginTransactionAsync(cancellationToken)).GetDbTransaction();
+    }
+}
--- Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
+++ Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -1,7 +1,14 @@
-﻿using Evently.Common.Presentation.Endpoints;
+﻿using Evently.Common.Infrastructure.Interceptors;
+using Evently.Common.Presentation.Endpoints;
+using Evently.Modules.Ticketing.Application.Abstractions.Data;
 using Evently.Modules.Ticketing.Application.Carts;
+using Evently.Modules.Ticketing.Domain.Customers;
+using Evently.Modules.Ticketing.Infrastructure.Customers;
+using Evently.Modules.Ticketing.Infrastructure.Database;
 using Evently.Modules.Ticketing.Presentation.Customers;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,7 +20,7 @@ public static class TicketingModule
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddInfrastructure();
+        services.AddInfrastructure(configuration);
 
         services.AddEndpoints(Presentation.AssemblyReference.Assembly);
 
@@ -25,8 +32,21 @@ public static class TicketingModule
         registrationConfigurator.AddConsumer<UserRegisteredIntegrationEventConsumer>();
     }
 
-    private static void AddInfrastructure(this IServiceCollection services)
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<TicketingDbContext>((sp, options) =>
+            options
+                .UseNpgsql(
+                    configuration.GetConnectionString("Database"),
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Ticketing))
+                .AddInterceptors(sp.GetRequiredService<PublishDomainEventsInterceptor>())
+                .UseSnakeCaseNamingConvention());
+
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<TicketingDbContext>());
+
         services.AddSingleton<CartService>();
     }
 }
